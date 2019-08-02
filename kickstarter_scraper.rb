c2ad2b6c4be465429b46042d7c8c48d26cd646cc
Andrew Bay <daddnbay@yahoo.com>
2019-08-02 20:52:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML
+  kickstarter = Nokogiri::HTML(html)
   
   projects = {}
   
