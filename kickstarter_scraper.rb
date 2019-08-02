94f207215a88ea61e40fedb017351849512e2b10
Andrew Bay <daddnbay@yahoo.com>
2019-08-02 20:38:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,8 +9,6 @@ require 'pry'
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
- 
-  
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {}
