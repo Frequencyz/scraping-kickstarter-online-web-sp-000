e957e1fecb0b3bbfdaf1b2a17a0f31d5ed1ce7eb
Andrew Bay <daddnbay@yahoo.com>
2019-08-02 20:38:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,13 +9,15 @@ require 'pry'
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
+  
+  
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {}
   end
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML
-  
+   projects = {}
   
   
   binding.pry
